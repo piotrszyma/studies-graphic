9eba295a09dd2ca6c907b089c571987ef7056f41
Piotr Szyma <thompson2908@gmail.com>
2018-04-07 19:24:20 +0200

    wireframe refactor

--- wireframe/app.js
+++ wireframe/app.js
@@ -4,11 +4,36 @@
 
 const SVG = document.querySelector('#main');
 
+let HEIGHT = SVG.clientHeight / 2;
+let WIDTH = SVG.clientWidth / 2;
+
+
+window.addEventListener('resize', () => {
+  HEIGHT = SVG.clientHeight / 2;
+  WIDTH = SVG.clientWidth / 2;
+  SVG.innerHTML = '';
+  
+drawFigure(outerFrameVertexes, 'outer-frame');
+drawFigure(playerBrickVertexes, 'player');
+
+getWallsVertexes().map(vertexPair => {
+  const [leftVertex, rightVertex] = vertexPair;
+  if (leftVertex) drawFigure(leftVertex, 'wall');  
+  if (rightVertex) drawFigure(rightVertex, 'wall');  
+});
+})
+
 // ================================
 //          DATA SECTION
 // ================================
 
-const MAX_DEPTH = 5;
+const MAX_DEPTH = 6;
+
+const WALL_THICKNESS = 0.5;
+
+const WIDTH_UNITS = 10;
+
+const WIDTH_UNIT = WIDTH / 5;
 
 const EDGES = [
   [0, 1],
@@ -26,43 +51,40 @@ const EDGES = [
 ]
 
 const outerFrameVertexes = [
-  [500, 250, 1], // 0 
-  [-500, 250, 1], // 1
-  [-500, -250, 1], // 2
-  [500, -250, 1], // 3
-  [500, 250, 8], // 4
-  [-500, 250, 8], // 5
-  [-500, -250, 8], // 6
-  [500, -250, 8], // 7
+  [WIDTH, HEIGHT, 1], // 0 
+  [-WIDTH, HEIGHT, 1], // 1
+  [-WIDTH, -HEIGHT, 1], // 2
+  [WIDTH, -HEIGHT, 1], // 3
+  [WIDTH, HEIGHT, MAX_DEPTH], // 4
+  [-WIDTH, HEIGHT, MAX_DEPTH], // 5
+  [-WIDTH, -HEIGHT, MAX_DEPTH], // 6
+  [WIDTH, -HEIGHT, MAX_DEPTH], // 7
 ]
 
 const playerBrickVertexes = [
-  [50, 250, 1], // 0 
-  [-50, 250, 1], // 1
-  [-50, 50, 1], // 2
-  [50, 50, 1], // 3
-  [50, 250, 1.5], // 4
-  [-50, 250, 1.5], // 5
-  [-50, 0, 1.5], // 6
-  [50, 0, 1.5], // 7
+  [WIDTH / WIDTH_UNITS,  HEIGHT / 2, 0.5], // 0 
+  [-WIDTH / WIDTH_UNITS, HEIGHT / 2, 0.5], // 1
+  [-WIDTH / WIDTH_UNITS, HEIGHT / 10, 0.5], // 2
+  [WIDTH / WIDTH_UNITS,  HEIGHT / 10, 0.5], // 3
+  [WIDTH / WIDTH_UNITS,  HEIGHT / 2, 0.5 + WALL_THICKNESS / 2], // 4
+  [-WIDTH / WIDTH_UNITS, HEIGHT / 2, 0.5 + WALL_THICKNESS / 2], // 5
+  [-WIDTH / WIDTH_UNITS, HEIGHT / 10, 0.5 + WALL_THICKNESS / 2], // 6
+  [WIDTH / WIDTH_UNITS,  HEIGHT / 10, 0.5 + WALL_THICKNESS / 2], // 7
 ]
 
 // start, end between 0 and 10
 // depth 2 - 8
 const generateVertex = (start, end, depth) => {
   return [
-    [end, 250, depth], // 0 
-    [start, 250, depth], // 1
-    [start, -250, depth], // 2
-    [end, -250, depth], // 3
-    [end, 250, depth + 0.5], // 4
-    [start, 250, depth + 0.5], // 5
-    [start, -250, depth + 0.5], // 6
-    [end, -250, depth + 0.5], // 7
-  ].map(vertex => {
-    vertex[0] = (vertex[0] - 5) * 100;
-    return [...vertex];
-  })
+    [end, HEIGHT, depth], // 0 
+    [start, HEIGHT, depth], // 1
+    [start, - HEIGHT, depth], // 2
+    [end, - HEIGHT, depth], // 3
+    [end, HEIGHT, depth + WALL_THICKNESS], // 4
+    [start, HEIGHT, depth + WALL_THICKNESS], // 5
+    [start, - HEIGHT, depth + WALL_THICKNESS], // 6
+    [end, -HEIGHT, depth + WALL_THICKNESS], // 7
+  ];
 }
 
 // ================================
@@ -94,17 +116,17 @@ const getColor = (type) => {
 const addLine = (x1, y1, x2, y2, type) => {
   requestAnimationFrame(() => {
     SVG.innerHTML += `<line 
-    x1="${x1 + 500}" 
-    y1="${y1  + 250}" 
-    x2="${x2 + 500}" 
-    y2="${y2 + 250}" 
+    x1="${x1 + WIDTH}" 
+    y1="${y1  + HEIGHT}" 
+    x2="${x2 + WIDTH}" 
+    y2="${y2 + HEIGHT}" 
     ${type ? 'class=' + type : ''}
     style="stroke: ${getColor(type)};">`
   })
 }
 
 const mapXYZtoXY = (x, y, z) => {
-  return [x / z, y / z];
+  return [x / ( z ), y / ( z )];
 }
 
 const drawFigure = (vertexes, type) => {
@@ -118,88 +140,127 @@ const drawFigure = (vertexes, type) => {
   });
 }
 
-const generateWalls = () => {
-  const holes = Array.from({
-    length: 4
-  }, () => Math.floor(Math.random() * 10));
+const getWallsVertexes = () => Array
+      .from({length: 4}, () => ~~(Math.random() * 4))
+      .map(h => {
+        if ( h === 0 ) {
+          return [null, [2, 10]]
+        } else if ( h === 9 ) {
+          return [[0, 8], null]
+        } else {
+          return [[0, h], [h + 2, 10]]      
+        }
+      }).map(p => {
+        const [left, right] = p
 
-  return holes.map((h, i) => {
-    const depth = i + 2;
-    const leftMax = h - 1;
-    const rightMin = h + 1;
-    const generatedLeftVertex = leftMax > 0 ? generateVertex(0, leftMax, depth) : null;
-    const generatedRightVertex = rightMin < 10 ? generateVertex(rightMin, 10, depth) : null;
-    const details = {
-      depth: depth,
-      left: h - 1,
-      right: h + 1
-    }
-    return [generatedLeftVertex, generatedRightVertex, details];
-  });
+        if ( left === null ) {
+          const [rightLeft, rightRight] = right;
+          return [null, [rightLeft * WIDTH_UNIT - WIDTH, rightRight * WIDTH_UNIT - WIDTH]];
+        } else if ( right === null ) {
+          const [leftLeft, leftRight] = left;
+          return [[leftLeft * WIDTH_UNIT - WIDTH, leftRight * WIDTH_UNIT - WIDTH], null];
+        } else {
+          const [leftLeft, leftRight] = left;
+          const [rightLeft, rightRight] = right;
+          return [
+            [leftLeft * WIDTH_UNIT - WIDTH, leftRight * WIDTH_UNIT - WIDTH],  
+            [rightLeft * WIDTH_UNIT - WIDTH, rightRight * WIDTH_UNIT - WIDTH]
+          ];
+        }
+      }).map((w, i) => {
+        const [left, right] = w;
 
-}
+          if ( left === null ) {
+          const [rightLeft, rightRight] = right;
+          return [null, generateVertex(rightLeft, rightRight, i + 1.5)];
+        } else if ( right === null ) {
+          const [leftLeft, leftRight] = left;
+          return [generateVertex(leftLeft, leftRight, i + 1.5), null];
+        } else {
+          const [leftLeft, leftRight] = left;
+          const [rightLeft, rightRight] = right;
+          return [
+            generateVertex(leftLeft, leftRight, i + 1.5),
+            generateVertex(rightLeft, rightRight, i + 1.5)
+          ];
+        }
+      });
+  // generate random holes
+  // calculate width of each wall
+  // return generated vertexes
 
 let DEPTH = playerBrickVertexes[0][2];
 
-document.addEventListener("keydown", (event) => {
-  console.log(playerBrickVertexes[0]);
+const AVAILABLE_KEYS = ['Up', 'Down', 'Left', 'Right'].map(s => `Arrow${s}`);
 
-  if (['ArrowRight', 'ArrowLeft'].includes(event.key)) {
-    if ('ArrowRight' === event.key) {
-      if (playerBrickVertexes[0][0] > 450) return;
-      document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
-      drawFigure(moveFigureLeftRight(playerBrickVertexes, 50), 'player');
-    } else {
-      if (playerBrickVertexes[0][0] < -350) return;
-      document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
-      drawFigure(moveFigureLeftRight(playerBrickVertexes, -50), 'player');
-    }
+document.addEventListener("keydown", (event) => {
+  if (!AVAILABLE_KEYS.includes(event.key)) return;
+
+  if ('ArrowRight' === event.key) {
+    if (playerBrickVertexes[0][0] === WIDTH / 2) return;
+    document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
+    drawFigure(moveFigureLeftRight(playerBrickVertexes, WIDTH / WIDTH_UNITS), 'player');
+  } else if('ArrowLeft' === event.key) {
+    if (playerBrickVertexes[1][0] === -WIDTH / 2) return;
+    document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
+    drawFigure(moveFigureLeftRight(playerBrickVertexes, -WIDTH / WIDTH_UNITS), 'player'); 
+  } else if ('ArrowUp' === event.key) {
+    if (playerBrickVertexes[5][2] === 3.0) return;    
+    document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
+    drawFigure(moveFigureForward(playerBrickVertexes, WALL_THICKNESS / 2), 'player');
+  } else if ('ArrowDown' === event.key) {
+    if (playerBrickVertexes[1][2] === 0.5) return;
+    document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
+    drawFigure(moveFigureForward(playerBrickVertexes, -WALL_THICKNESS / 2), 'player');
   }
 });
 
 drawFigure(outerFrameVertexes, 'outer-frame');
 drawFigure(playerBrickVertexes, 'player');
 
-const walls = generateWalls();
+getWallsVertexes().map(vertexPair => {
+  const [leftVertex, rightVertex] = vertexPair;
+  if (leftVertex) drawFigure(leftVertex, 'wall');  
+  if (rightVertex) drawFigure(rightVertex, 'wall');  
+});
 
-const MAP_DETAILS = walls.map((o, i) => {
-  const [left, right, details] = o;
-  if (left) drawFigure(left, `wall-${i}`);
-  if (right) drawFigure(right, `wall-${i}`);
-  return details
-})
+// const walls = generateWalls();
 
-setInterval(() => {
-  document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
-  drawFigure(moveFigureForward(playerBrickVertexes, 0.5), 'player');
+// const MAP_DETAILS = walls.map((o, i) => {
+//   const [left, right, details] = o;
+//   if (left) drawFigure(left, `wall wall-${i}`);
+//   if (right) drawFigure(right, `wall wall-${i}`);
+//   return details
+// })
 
-  const START = playerBrickVertexes[1][0];
-  const END = playerBrickVertexes[0][0];
-  const DEPTH = playerBrickVertexes[0][2];
-  if (DEPTH === ~~DEPTH) {
-    const {
-      left,
-      right,
-      depth
-    } = MAP_DETAILS.find(o => o.depth === DEPTH);
-    const [objLeft, objRight] = [(START + 500) / 100, (END + 500) / 100]
+// setInterval(() => {
+//   document.querySelectorAll('.player').forEach(o => o.parentNode.removeChild(o));
 
-    if (objLeft < left || objRight > right) {
-      alert("You lost");
-      location.reload();
-    } else {
-      const wallsToColor = document.querySelectorAll(`.wall-${DEPTH - 2}`)
-      wallsToColor.forEach(o => {
-        o.style = 'opacity: 0;'
-      })
+//   const START = playerBrickVertexes[1][0];
+//   const END = playerBrickVertexes[0][0];
+//   const DEPTH = playerBrickVertexes[0][2];
 
-      if (DEPTH === MAX_DEPTH) {
-        alert("You won");
-        location.reload();
-      }
-    }
-  }
+//   if (DEPTH === ~~DEPTH) {
+//     const {
+//       left,
+//       right,
+//       depth
+//     } = MAP_DETAILS.find(o => o.depth === DEPTH);
+//     const [objLeft, objRight] = [(START + 500) / 100, (END + 500) / 100]
 
+//     console.log(DEPTH);
 
-
-}, 1000);
+//     if (objLeft < left || objRight > right) {
+//       // alert("You lost");
+//       // location.reload();
+//     } else {
+//       const wallsToColor = document.querySelectorAll(`.wall-${DEPTH - 2}`)
+//       wallsToColor.forEach(o => {
+//         o.style = 'opacity: 0;'
+//       })
+//       if (DEPTH > MAX_DEPTH) {
+//         // location.reload();
+//       }
+//     }
+//   }
+// }, 1000);
