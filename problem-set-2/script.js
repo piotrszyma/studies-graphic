b0465230a0b57f3811c6912f8fc7217440f32123
Piotr Szyma <thompson2908@gmail.com>
2018-05-04 18:19:15 +0200

    triangle via elements

--- problem-set-2/script.js
+++ problem-set-2/script.js
@@ -9,10 +9,12 @@ const canvas = document.querySelector('canvas');
 const gl = canvas.getContext('webgl');
 
 const vertices = [
-  -0.5 ,  0.5 ,  0.0,
-   0.0 ,  0.5 ,  0.0, 
-  -0.25,  0.25,  0.0, 
+  -0.5,  0.5,  0.0,
+  -0.5, -0.5,  0.0,
+   0.5, -0.5,  0.0, 
 ];
+
+const indices = [0, 1, 2];
 
 // ==========================================
 //
@@ -32,6 +34,11 @@ gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 // Unbind the buffer
 gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
+const indexBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
 // ==========================================
 //
 //   Create and compile Shader programs
@@ -45,7 +52,6 @@ const vertexShaderCode = `
   attribute vec3 coordinates;
   void main(void) {
     gl_Position = vec4(coordinates, 1.0);
-    gl_PointSize = 10.0;
   }
 `
 gl.shaderSource(vertexShader, vertexShaderCode);
@@ -84,11 +90,13 @@ gl.useProgram(shaderProgram);
 
 gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
 
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+
 const coordinates = gl.getAttribLocation(shaderProgram, 'coordinates');
 
 // Point an attribute to the currently bound Vertex Buffer Object
 
-gl.vertexAttribPointer(coordinates, 2, gl.FLOAT, false, 0, 0);
+gl.vertexAttribPointer(coordinates, 3, gl.FLOAT, false, 0, 0);
 
 // Enable the attribute
 
@@ -109,4 +117,4 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 
 gl.viewport(0, 0, canvas.width, canvas.height);
 
-gl.drawArrays(gl.POINTS, 0, 3);
+gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
